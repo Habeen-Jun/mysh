f7224cb4241b45f84851b5afec65b250b7205e6d
Yunhyuk Chang <yc1128@python.cs.rutgers.edu>
2023-03-24 16:03:36 -0400

    wildcard, traversing

--- mysh.c
+++ mysh.c
@@ -6,6 +6,7 @@
 #include "words.h"
 #include <unistd.h>
 #include <sys/stat.h>
+#include <dirent.h>
 
 #define PROMPT "mysh> "
 #define IS_EXCEPTIONS(x) (x == '|' | x == '<' | x == '>')
@@ -85,6 +86,71 @@ char *get_executable_path(char *word) {
     
 }
 
+char** traverse(char *dname, char** sub_files, size_t* size) {
+    struct dirent *de;
+    long offset;
+    int flen;
+    int dlen = strlen(dname);
+    char *pname;
+
+    DIR *dp = opendir(dname);
+
+    // char *sub_files = malloc(sizeof(char) * (size + 1));
+    // char **sub_files = malloc(sizeof(char *));
+    
+    if (!dp) {
+        perror(dname);
+        return NULL;
+        // return;
+    }
+    // printf("Traversing %s\n", dname);
+
+    int i = 0;
+    
+    while ((de = readdir(dp))) {
+        // printing out all the files/subdirectoreis inside the directory
+        // printf("%s/%s\n", dname, de->d_name);
+        if (de->d_type == DT_DIR && de->d_name[0] != '.') {
+            // construct new path
+            flen = strlen(de->d_name);
+            pname = malloc(dlen + flen + 2);
+            memcpy(pname, dname, dlen);
+            pname[dlen] = '/';
+            memcpy(pname + dlen + 1, de->d_name, flen);
+            pname[dlen + 1 + flen] = '\0';
+            // save location in directory
+            offset = telldir(dp);
+            closedir(dp);
+            // recursively traverse subdirectory
+            sub_files = traverse(pname, sub_files, size);
+            free(pname);
+            // restore position in directory
+            dp = opendir(dname); // FIXME: check for success
+            seekdir(dp, offset);
+        }
+        else/* if(de->d_type == DT_REG) */{
+            char *file_name = malloc(sizeof(de->d_name)+sizeof("./"));
+            strcpy(file_name, "./"); // all file starts with "./""
+            strcat(file_name, de->d_name);
+            // printf("%s", slash);
+            int len = strlen(file_name);
+            // printf("%s and %d, i: %d\n", file_name, len, i);
+
+            // printf("%ld\n", *size);
+            sub_files[*size] = file_name;
+            *size = *size + 1;
+            sub_files = (char**) realloc(sub_files, sizeof(char *)*(*size + 1)); // resize sub_files array to hold i + 1 pointers
+            
+            i++;
+        }
+    }
+    // printf("%p %p\n", readdir(dp), dp);
+    // printf("%ld\n", *size);
+    closedir(dp);
+    return sub_files;
+}
+
+
 void print_string_list(char **list, int num_strings) {
     int i;
     // fflush(stdout);
@@ -145,6 +211,105 @@ struct exec_info* parseCommand() {
             word = words_next();
             input_file = malloc(sizeof(char) * strlen(word) + 1);
             strcpy(input_file, word);
+
+        } else if (strchr(word, '*') != NULL) { // check if the token contains "*"
+            char delim[] = "*"; // The character on the basis of which the split will be done
+            char *token = strtok(word, delim);
+
+            // printf("%s\n", token);
+
+            int i = 0;
+
+            char *pattern[2];
+
+            // printf("%d\n", word[0]);
+
+            // splitting the token and save it
+            // do I need this if/else if statement?
+            if(word[0] == '*') {
+                pattern[0] = NULL;
+                pattern[1] = token;
+            }
+            else {
+                while(token != NULL) {
+                    pattern[i] = token;
+                    // printf("%s \n", pattern[i]);
+                    token = strtok(NULL, delim); // this gives the next remaining string
+                    i++;
+                }
+            }
+
+            // printf("pattern[0] = %s, pattern[1] = %s \n", pattern[0], pattern[1]);
+
+            // traversing and storing all the file name
+            char **files = malloc(sizeof(char *));
+            size_t num = 0;
+            size_t *size = &num;
+
+            files = traverse(".", files, size); // stores all the subdirectory/file name
+            
+            // divide the cases of the pattern
+            
+            // check all file name that has certain pattern required
+            // use strstr() to compare
+            char *first_pattern;
+            char *second_pattern;
+
+            char **replace_files = malloc(sizeof(char *));
+            
+
+            // checks the first pattern is in the file name (when it only has front)
+            if(pattern[0] != NULL) {
+                for(int i = 0; i < *size; i++) {
+                    first_pattern = strstr(files[i], pattern[0]);
+                    if(first_pattern != NULL) {
+                        printf("%s \n", files[i]);
+                        second_pattern = strstr(first_pattern, pattern[1]);
+                        // checking the pattern when it has string both front and back
+                        if(second_pattern != NULL) {
+                            printf("%s \n", files[i]);
+                            // replace_files = realloc(replace_files, (i+1));
+                            // return replace_files;
+                        }
+                    }
+
+                    // else if(first_pattern == NULL) {
+                    //     // printf("No match \n");
+                    // }
+                }
+            }
+            // when it has only back
+            else {
+                for(int i = 0; i < *size; i++) {
+                    second_pattern = strstr(files[i], pattern[1]);
+                    if(second_pattern != NULL) {
+                        printf("%s \n", files[i]);
+                    }
+                }
+            }
+
+            // for(int i = 0; i < *size; i++) {
+            //     first_pattern = strstr(files[i], pattern[0]);
+            //     second_pattern = strstr(files[i], pattern[1]);
+
+            //     // checks if it contains both pattern in order (first_pattern comes first)
+            //     if(first_pattern != NULL && second_pattern != NULL && first_pattern < second_pattern) {
+            //         // contains string in the front and back of "*"
+            //         printf("%s\n", files[i]);
+            //     }
+            //     else if(first_pattern == NULL && second_pattern != NULL) {
+                    
+            //         printf("%s\n", files[i]);
+            //     }
+            // }
+
+            // for(int i = 0; i < *size; i++){
+            //     // printf("%s\n", files[i]);
+            //     if(files[i])
+            // }
+
+            
+
         } else {
             // arguments
             // printf("args\n");
@@ -154,6 +319,7 @@ struct exec_info* parseCommand() {
             strcpy(arguments[args_pos], word);
             
             ++args_pos;
+
         }
 
         free(word);
@@ -271,6 +437,7 @@ void batchMode(char *fname) {
     }
     close(fd);
 }
+
  
 int main(int argc, char **argv)
 {
@@ -284,7 +451,18 @@ int main(int argc, char **argv)
         batchMode(argv[1]);
     }
 
-    // batch mode
+    // char **sub_files = malloc(sizeof(char *));
+    // size_t num = 0;
+    // size_t *size = &num;
 
+    // printf("%ld\n", *size);
+
+    // sub_files = traverse(".", sub_files, size);
+
+    // printf("%ld\n", *size);
+
+    // for(int i = 0; i < *size; i++){
+    //     printf("%s\n", sub_files[i]);
+    // }
 
 }
